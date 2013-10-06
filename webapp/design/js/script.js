f7be6a48a661f6203bbb09ebaf733311541f0bcd
Prudhon <sprudhon11@gmail.com>
2013-10-06 17:47:27 +0200

    sécurité création recette terminée sécurité inscription terminée (le regex fonctionne [-enfin-])

--- webapp/design/js/script.js
+++ webapp/design/js/script.js
@@ -2,12 +2,16 @@ $(document).ready(function(){
 
 // Security new user's form
 	$("#envoieFormUser").click(function(){
-		valid =0;
-		vmail=0;
-		vpwd=0;
-		vcpwd =0;
+		var valid =0;
+		var vmail=0;
+		var vpwd=0;
+		var vcpwd =0;
+
+		var inputVal = $("#mail").val();
+		var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+
 		// verif mail
-		if(($("#mail").val() == "") /*|| ($("#mail").match(/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/i))*/){
+		if(   ($("#mail").val() == "") || (!emailReg.test(inputVal))  ){
 			$("#mail").css("border-color","#ff0000");
 			$("#labMail").css("color", "#ff0000");
 			vmail ++;
@@ -66,8 +70,86 @@ $(document).ready(function(){
 
 // Security Recipe
 	$("#envoieRecette").click(function(){
-		// alert($(".inputForm").is(":empty") );
-		});
+		var vPhoto =0;
+		var vPersonne =0;
+		var vPrep =0;
+		var vCuisson =0;
+		var vNom =0;
+		var vEtape =0;
+
+		var numericReg = /^\d*[0-9](|.\d*[0-9]|,\d*[0-9])?$/;
+		/* Verif photo choisie */
+		// picture validation
+		if ($('#Rphoto').val() == '')
+		{
+			$('#labelRphoto').css("color","#f00");
+			vPhoto ++;
+		}
+		else
+		{
+			$('#labelRphoto').css("color","#000");
+			vPhoto =0;
+		}
+		/* Verif string*/
+		// verif nom
+		if($("#Rnom").val().length < 5){
+			$("#labelRnom").css("color", "#ff0000");
+			vNom ++;
+		}
+		else
+		{
+			$("#labelRnom").css("color", "#000");
+			vNom = 0;
+		}
+		// verif etape
+		if($("#Retape").val().length < 15){
+			$("#labelRetape").css("color", "#ff0000");
+			vEtape ++;
+		}
+		else
+		{
+			$("#labelRetape").css("color", "#000");
+			vEtape = 0;
+		}
+		/* Verif numérique */
+		// verif nb personne
+		if(!numericReg.test($("#Rpersonne").val())) {
+        	$('#labelRpersonne').css("color","#f00");
+			vPersonne ++;
+		}
+		else
+		{
+			$('#labelRpersonne').css("color","#000");
+			vPersonne =0;
+		}
+		// verif preparation time
+		if(!numericReg.test($("#Rprep").val())) {
+        	$('#labelRprep').css("color","#f00");
+			vPrep ++;
+		}
+		else
+		{
+			$('#labelRprep').css("color","#000");
+			vPrep =0;
+		}
+		// verif cuisson time
+		if(!numericReg.test($("#Rcuisson").val())) {
+        	$('#labelRcuisson').css("color","#f00");
+			vCuisson ++;
+		}
+		else
+		{
+			$('#labelRcuisson').css("color","#000");
+			vCuisson =0;
+		}				
+		// autorisation
+		valide = vPhoto + vPersonne + vPrep + vCuisson + vNom + vEtape;
+		alert(valide);
+		if (valide == 0)
+			return true;
+		else
+			return false;
+	});
 
 
 
